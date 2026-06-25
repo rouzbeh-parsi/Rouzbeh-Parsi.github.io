61dd569e7dd37f5fc5507f28c88ce217466fd3a1
rouzbeh-parsi <132729217+rouzbeh-parsi@users.noreply.github.com>
2026-06-25 14:25:57 -0700

    Update and rename BC_forecast.html to BC_forecast.js

---
+++ assets/js/BC_forecast.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+var historical = {
+    x: [2018,2019,2020,2021,2022,2023,2024,2025],
+    y: [17,18,19,20,21,22,23,24],
+    mode: 'lines+markers',
+    name: 'Historical'
+};
+
+var forecast = {
+    x: [2026,2027,2028],
+    y: [25,26,27],
+    mode: 'lines+markers',
+    name: 'Forecast'
+};
+
+var upper = {
+    x: [2026,2027,2028],
+    y: [26,27,28],
+    mode: 'lines',
+    line: {dash:'dot'},
+    name:'Upper Bound'
+};
+
+var lower = {
+    x: [2026,2027,2028],
+    y: [24,25,26],
+    mode:'lines',
+    line:{dash:'dot'},
+    name:'Lower Bound'
+};
+
+Plotly.newPlot(
+    'chart',
+    [historical, forecast, upper, lower],
+    {
+        title:'BC Personal Income Tax Forecast',
+        hovermode:'x unified'
+    }
+);
+
+});
